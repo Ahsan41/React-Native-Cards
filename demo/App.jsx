3afa2d16d3b54fa2baf10b1085237b2a722b67ef
Ahsan41 <119751735+Ahsan41@users.noreply.github.com>
2023-11-10 17:38:14 +0500

    first native project

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,45 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { Dimensions, StyleSheet, Text, View, ScrollView , Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, StyleSheet, Text, View, ScrollView, Image, TextInput } from 'react-native';
 
 export default function App() {
-  const {height,width} = Dimensions.get('screen')
-  const [movies, setMovies] = useState
-  ([]);
+  const { height, width } = Dimensions.get('screen');
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const res = await fetch(`https://www.omdbapi.com/?apikey=edcaa7a5&s=marvel`); // Removed backticks
-        const data = await res.json();
-        console.log(data.Search);
-        setMovies(data.Search);
+        const response = await fetch(`https://www.omdbapi.com/?apikey=edcaa7a5&s=${search}`);
+        const data = await response.json();
+
+        if (data.Search) {
+          setMovies(data.Search);
+        } else {
+          setMovies([]);
+        }
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchMovies();
-  }, []);
+  }, [search]);
+
   return (
     <View style={styles.container}>
-
       <ScrollView>
-      <Text style={styles.Text}> 10 movies found in this list</Text>
+        <Text style={styles.Text}>{movies.length} movies found in this list</Text>
 
-   {movies.map((movie)=>(
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => setSearch(text)}
+          placeholder="Search for movies..."
+        />
 
-     
-     <View style={styles.moviesList} key={movie.imbdID}>
+        {movies.map((movie) => (
+          <View style={styles.moviesList} key={movie.imdbID}>
             <Image
-            style={{width:250,height:200,display:'flex',alignItems:'center',objectFit:'cover',flexWrap:'wrap', backgroundColor:"#fff"}}
-            source= {movie.Poster}/>
-            <Text style={{color:'gold', marginTop:20, fontSize:15 }} >{movie.Title}</Text>
+              style={{
+                width: 250,
+                height: 200,
+                display: 'flex',
+                alignItems: 'center',
+                objectFit: 'cover',
+                flexWrap: 'wrap',
+                backgroundColor: '#fff',
+              }}
+              source={{ uri: movie.Poster }}
+            />
+            <Text style={{ color: 'gold', marginTop: 20, fontSize: 15 }}>{movie.Title}</Text>
           </View>
-            ))}
-        
-
-      </ScrollView>      
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -47,21 +61,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#000",
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
   Text: {
-    color:'gold',
-    fontWeight:'bold',
-    marginTop:50,
-    fontSize:20,
-    textAlign:'center'
-    
+    color: 'gold',
+    fontWeight: 'bold',
+    marginTop: 50,
+    fontSize: 20,
+    textAlign: 'center',
   },
-  moviesList:{
-    display:'flex',
-    alignItems:'center',
-    marginTop:40,
-  }
+  moviesList: {
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  input: {
+    height: 40,
+    borderColor: 'grey',
+    borderWidth: 1,
+    marginTop: 10,
+    paddingHorizontal: 8,
+    color: 'black',
+    backgroundColor: 'gold',
+    marginTop: 30,
+    padding: 20,
+    marginLeft: 25,
+    marginRight: 25,
+  },
 });
